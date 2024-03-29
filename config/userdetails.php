6ef9aef2b4e3c08d160ab63380aeacb8fddf445f
Drew0004 <drdrewgraphic@gmail.com>
2024-03-29 16:37:41 +0100

    Add userDetails config and fix users seeder

---
+++ config/userdetails.php
@@ -0,0 +1,56 @@
+<?php
+   return[
+        [
+            'id' => '1',
+            'demo' => 'demo prova ',
+            'picture' => 'Mario_Cilino.jpg',
+            'bio' => "Sono Mario Cilino, un musicista italiano con una passione travolgente per
+            la creazione musicale. Fin da giovane, ho coltivato il mio talento, imparando a suonare
+            diversi strumenti e sviluppando un amore profondo per la composizione.",
+            'cellphone' => '+39 3385698137',
+            'members' => 'Polistrumentista e amante del surf'
+        ],
+        [
+            'id' => '2',
+            'demo' => 'demo prova ',
+            'picture' => 'Paola_Gagliardi.jpg',
+            'bio' => "Ciao, sono Paola Gagliardi, una cantante italiana appassionata.
+            La mia voce è il mio strumento principale, attraverso il quale trasmetto
+            emozioni profonde e connetto con il mio pubblico. La mia musica abbraccia
+            una varietà di generi, dal pop all'R&B.",
+            'cellphone' => '+39 3385648567',
+            'members' => 'Amante della cultura americana e ricercatrice universitaria'
+        ],
+        [
+            'id' => '3',
+            'demo' => 'demo prova ',
+            'picture' => 'The_Rock_Army.jpg',
+            'bio' => "Siamo The Rock Army, una band dedicata al rock che trasmette
+            energia e potenza attraverso i nostri riff incisivi e i ritmi travolgenti.
+            Ogni membro porta la propria energia unica al nostro suono, creando un mix
+            eclettico di stili e influenze rock.",
+            'cellphone' => '+39 3382135137',
+            'members' => 'Giacomo Francesco e Mattia. Ci siamo conosciuti sui banchi di scuola '
+        ],
+        [
+            'id' => '4',
+            'demo' => 'demo prova ',
+            'picture' => 'Nathan_Drake.jpg',
+            'bio' => "Ciao, sono Nathan Drake, un cantante dallo spirito avventuroso.
+            La mia musica è come una ricerca del tesoro, piena di emozioni e avventure che portano
+            l'ascoltatore in luoghi lontani e inesplorati. ",
+            'cellphone' => '+39 3364398137',
+            'members' => 'Vivo da 15 anni in Italia portando le mie esperienze di vita sul palco'
+        ],
+        [
+            'id' => '5',
+            'demo' => 'demo prova ',
+            'picture' => 'Jazzy_Sons.jpg',
+            'bio' => 'Siamo i Jazzy Sons, una band che porta un tocco
+            fresco e moderno al jazz. Con melodie sofisticate e improvvisazioni
+            vibranti, ci esibiamo con energia e passione per portare il jazz
+            nelle vite di chi ci ascolta.',
+            'cellphone' => '+39 3327698186',
+            'members' => 'Suoniamo dal giorno in cui ci siamo conosciuti ad Amsterdam e giriamo per lo stivale'
+        ],
+   ];
