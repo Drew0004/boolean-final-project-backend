f968c84ee4d0de91d5b69d29b033b7b088f7671c
Drew0004 <drdrewgraphic@gmail.com>
2024-03-29 16:44:14 +0100

    Add reviews config and fix reviews seeder

---
+++ config/reviews.php
@@ -0,0 +1,47 @@
+<?php
+   return[
+    [
+        'firstname'=>'Alberto',
+        'lastname'=>'Benvenuti',
+        'description'=> 'Sono rimasto letteralmente senza fiato dopo aver assistito a questo concerto.
+        Interazione con il pubblico incredibile.
+        Ogni nota suonata è stata carica di emozione e passione.
+        È stato uno spettacolo indimenticabile che mi ha lasciato
+        desideroso di più. Assolutamente da vedere!',
+    ],
+    [
+        'firstname'=>'Federico',
+        'lastname'=>'Aliquò',
+        'description'=> 'Questo concerto ha creato delle atmosfere magiche che mi hanno rapito fin dal primo momento.
+        Il perfomer ha saputo coinvolgere il pubblico con la sua presenza carismatica e
+        le sue performance straordinarie. La musica ha trasportato tutti in un viaggio
+        emotivo che non dimenticherò facilmente.',
+    ],
+    [
+        'firstname'=>'Marco',
+        'lastname'=>'Barni',
+        'description'=> 'Sfortunatamente, questo concerto è stato una vera delusione.
+        Le canzoni suonate erano prive di energia e sembrava che mancasse qualsiasi
+        tipo di connessione emotiva. È stato un vero peccato perché avevo grandi aspettative,
+        ma alla fine sono rimasto deluso dalla serata',
+    ],
+    [
+        'firstname'=>'Leonardo',
+        'lastname'=>'Parisi',
+        'description'=> "Questo concerto è stato semplicemente epico! L'artista ha
+        dato il massimo di sé sul palco, regalando al pubblico una performance straordinaria.
+        Le luci, il suono e l'atmosfera generale erano perfetti.
+        È stata una serata che rimarrà impressa nella mia memoria per
+        sempre. Non vedo l'ora di rivivere un'esperienza simile in futuro!",
+    ],
+    [
+        'firstname'=>'Leonardo',
+        'lastname'=>'Parisi',
+        'description'=> "Purtroppo, questo concerto è stato un completo
+        fallimento. L'organizzazione è stata disastrosa e l'artista
+        sembrava completamente fuori forma. Il suono era terribile e
+        l'atmosfera era cupa e deprimente. È stato un vero spreco di
+        tempo e denaro. Consiglio vivamente di evitare questo tipo di
+        eventi in futuro.",
+    ],
+   ];
