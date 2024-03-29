0a9fdae2587ca588bc9c777a79ae9bf239427215
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-03-29 17:26:08 +0100

    Add config Udetail & messages

--- config/userdetails.php
+++ config/userdetails.php
@@ -53,4 +53,124 @@
             'cellphone' => '+39 3327698186',
             'members' => 'Suoniamo dal giorno in cui ci siamo conosciuti ad Amsterdam e giriamo per lo stivale'
         ],
+        [
+            'id' => '6',
+            'demo' => 'demo performance',
+            'picture' => 'Rolling_Rocks.jpg',
+            'bio' => 'Siamo i Rolling Rocks, una band che si ispira allo spirito ribelle e travolgente delle Pietre Rotolanti. Con riff taglienti e performance adrenaliniche, portiamo l\'energia del rock sul palco in ogni concerto che facciamo. I nostri membri sono musicisti appassionati che condividono una passione comune per il rock \'n\' roll. Suoniamo insieme da anni, portando il nostro sound potente e coinvolgente in ogni luogo in cui ci esibiamo.',
+            'cellphone' => '+39 3374562398',
+            'members' => 'La nostra band è composta da talentuosi musicisti che portano una vasta esperienza sul palco e una passione travolgente per la musica rock. Ogni membro contribuisce con il proprio stile unico, creando un sound coeso e coinvolgente.'
+        ],
+        [
+            'id' => '7',
+            'demo' => 'demo jazz',
+            'picture' => 'Jazzy_Trip.jpg',
+            'bio' => 'Siamo i Jazzy Trip, una band che si ispira allo spirito avventuroso e affascinante dei Viaggi Jazz. Con melodie fluide e improvvisazioni brillanti, portiamo il fascino del jazz su ogni palco che calciamo. I nostri membri sono musicisti appassionati che condividono una profonda connessione con il jazz. Suoniamo insieme da anni, portando il nostro sound unico in ogni luogo in cui ci esibiamo.',
+            'cellphone' => '+39 3385679241',
+            'members' => 'La nostra band è composta da talentuosi musicisti che portano una vasta esperienza nel mondo del jazz. Ogni membro contribuisce con il proprio talento e creatività, creando un sound avvincente e coinvolgente.'
+        ],
+        [
+            'id' => '8',
+            'demo' => 'demo elettrico',
+            'picture' => 'Electric_Dreams.jpg',
+            'bio' => 'Siamo gli Electric Dreams, una band ispirata allo spirito elettrizzante dei Sogni Elettrici. Con ritmi pulsanti e suoni energici, portiamo l\'elettricità della musica su ogni palco che calciamo. I nostri membri sono musicisti appassionati che condividono una profonda connessione con la musica elettronica. Suoniamo insieme da anni, portando il nostro sound innovativo in ogni luogo in cui ci esibiamo.',
+            'cellphone' => '+39 3396547812',
+            'members' => 'La nostra band è composta da talentuosi musicisti che portano una vasta esperienza nel mondo della musica elettronica. Ogni membro contribuisce con il proprio stile unico, creando un sound avvincente e coinvolgente.'
+        ],
+        [
+            'id' => '9',
+            'demo' => 'demo funky',
+            'picture' => 'Funky_Express.jpg',
+            'bio' => 'Siamo i Funky Express, una band che si ispira allo spirito vivace ed energico dell\'Espresso Funk. Con groove irresistibili e ritmi travolgenti, portiamo il funk su ogni palco che calciamo. I nostri membri sono musicisti appassionati che portano una vasta esperienza nel mondo della musica funk. Suoniamo insieme da anni, portando il nostro sound scatenato in ogni luogo in cui ci esibiamo.',
+            'cellphone' => '+39 3409876231',
+            'members' => 'La nostra band è composta da talentuosi musicisti che portano una vasta esperienza nel mondo del funk. Ogni membro contribuisce con il proprio talento e creatività, creando un sound coinvolgente e travolgente.'
+        ],
+        [
+            'id' => '10',
+            'demo' => 'demo classico',
+            'picture' => 'Classic_Vibes.jpg',
+            'bio' => 'Siamo i Classic Vibes, una band che si ispira allo spirito etereo ed elegante degli Echi Classici. Con melodie raffinate e armonie avvolgenti, portiamo il fascino della musica classica su ogni palco che calciamo. I nostri membri sono musicisti appassionati che portano una vasta esperienza nel mondo della musica classica. Suoniamo insieme da anni, portando il nostro sound raffinato in ogni luogo in cui ci esibiamo.',
+            'cellphone' => '+39 3418765923',
+            'members' => 'La nostra band è composta da talentuosi musicisti che portano una vasta esperienza nel mondo della musica classica. Ogni membro contribuisce con il proprio virtuosismo e sensibilità musicale, creando un sound unico e suggestivo.'
+        ],
+        [
+            'id' => '11',
+            'demo' => 'demo reggae',
+            'picture' => 'Reggae_Rhythms.jpg',
+            'bio' => 'Siamo i Reggae Rhythms, una band ispirata allo spirito rilassato e positivo dei Ritmi Reggae. Con ritmi incalzanti e melodie coinvolgenti, portiamo il calore della musica reggae su ogni palco che calciamo. I nostri membri sono musicisti appassionati che portano una vasta esperienza nel mondo del reggae. Suoniamo insieme da anni, portando il nostro sound vibrante in ogni luogo in cui ci esibiamo.',
+            'cellphone' => '+39 3461237890',
+            'members' => 'La nostra band è composta da talentuosi musicisti che portano una vasta esperienza nel mondo del reggae. Ogni membro contribuisce con il proprio stile unico, creando un sound coinvolgente e positivo.'
+        ],
+        [
+            'id' => '12',
+            'demo' => 'demo solista',
+            'picture' => 'Solo_Performer.jpg',
+            'bio' => 'Sono Gianluca Rossi, un musicista solista con una passione travolgente per la musica. Con il mio talento e la mia creatività, porto la magia della musica su ogni palco che calcio. Ho una vasta esperienza nel mondo della musica e sono pronto a emozionare il pubblico con le mie performance coinvolgenti.',
+            'cellphone' => '+39 3472345678',
+            'members' => ''
+        ],
+        [
+            'id' => '13',
+            'demo' => 'demo solista',
+            'picture' => 'Solo_Performer.jpg',
+            'bio' => 'Sono Eros Leonardi, un musicista solista con una passione travolgente per la musica. Con il mio talento e la mia creatività, porto la magia della musica su ogni palco che calcio. Ho una vasta esperienza nel mondo della musica e sono pronto a emozionare il pubblico con le mie performance coinvolgenti.',
+            'cellphone' => '+39 3483456789',
+            'members' => ''
+        ],
+        [
+            'id' => '14',
+            'demo' => 'demo solista',
+            'picture' => 'Solo_Performer.jpg',
+            'bio' => 'Sono Laura Paoletti, una musicista solista con una passione travolgente per la musica. Con il mio talento e la mia creatività, porto la magia della musica su ogni palco che calcio. Ho una vasta esperienza nel mondo della musica e sono pronta a emozionare il pubblico con le mie performance coinvolgenti.',
+            'cellphone' => '+39 3494567890',
+            'members' => ''
+        ],
+        [
+            'id' => '15',
+            'demo' => 'demo solista',
+            'picture' => 'Solo_Performer.jpg',
+            'bio' => 'Sono Marco Ricci, un musicista solista con una passione travolgente per la musica. Con il mio talento e la mia creatività, porto la magia della musica su ogni palco che calcio. Ho una vasta esperienza nel mondo della musica e sono pronto a emozionare il pubblico con le mie performance coinvolgenti.',
+            'cellphone' => '+39 3505678901',
+            'members' => ''
+        ],
+        [
+            'id' => '16',
+            'demo' => 'demo solista',
+            'picture' => 'Solo_Performer.jpg',
+            'bio' => "Sono Alessandro Conti, un musicista solista con una passione travolgente per la musica. Con il mio talento e la mia creatività, porto la magia della musica su ogni palco che calcio. Ho una vasta esperienza nel mondo della musica e sono pronto a emozionare il pubblico con le mie performance coinvolgenti.",
+            'cellphone' => '+39 3516789012',
+            'members' => ''
+        ],
+        [
+            'id' => '17',
+            'demo' => 'demo solista',
+            'picture' => 'Solo_Performer.jpg',
+            'bio' => "Sono Simone Moretti, un cantautore appassionato e talentuoso che porta la propria unicità e creatività nella musica che scrive e interpreta. Con esperienza pluriennale nel campo della musica, ho il piacere di emozionare il pubblico con le mie canzoni coinvolgenti e ricche di significato.",
+            'cellphone' => '+39 3527890123',
+            'members' => ''
+        ],
+        [
+            'id' => '18',
+            'demo' => 'demo solista',
+            'picture' => 'Solo_Performer.jpg',
+            'bio' => "Francesco Ferrari è un musicista solista con una passione travolgente per la creazione e l'interpretazione musicale. Con un background artistico diversificato e una vasta gamma di influenze musicali, porto una fresca e innovativa prospettiva nella scena musicale. Con le mie performance coinvolgenti e autentiche, cerco di connettere profondamente con il pubblico attraverso la mia musica.",
+            'cellphone' => '+39 3538901234',
+            'members' => ''
+        ],
+        [
+            'id' => '19',
+            'demo' => 'demo solista',
+            'picture' => 'Solo_Performer.jpg',
+            'bio' => "Anna Romano è una cantante e compositrice appassionata che cattura l'essenza delle emozioni attraverso la musica. Con una voce potente e versatile e testi coinvolgenti, porto il mio pubblico in un viaggio attraverso le esperienze umane. Le mie performance dal vivo sono un mix di energia e sensibilità, creando un'esperienza indimenticabile per chi ascolta.",
+            'cellphone' => '+39 3549012345',
+            'members' => ''
+        ],
+        [
+            'id' => '20',
+            'demo' => 'demo solista',
+            'picture' => 'Solo_Performer.jpg',
+            'bio' => "Roberto Esposito è un cantautore e musicista che porta il suo cuore e la sua anima nella musica che crea. Con uno stile unico e autentico, trasmetto emozioni e storie attraverso le mie canzoni. Con le mie performance dal vivo, cerco di connettere con il pubblico e lasciare un'impronta duratura nel cuore di chi mi ascolta.",
+            'cellphone' => '+39 3550123456',
+            'members' => ''
+        ]
    ];
