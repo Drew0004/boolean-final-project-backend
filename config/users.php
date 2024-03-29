70e284b0c0768c93b81a096ffb05aba133283f14
Drew0004 <drdrewgraphic@gmail.com>
2024-03-29 16:27:58 +0100

    Add users config and fix users seeder

---
+++ config/users.php
@@ -0,0 +1,33 @@
+<?php
+   return[
+        [
+            'name' => 'Mario Cilino',
+            'email' => 'mario.cilino@gmail.com',
+            'password' => 'mariocilino30',
+            'city' => 'Milano'
+        ],
+        [
+            'name' => 'Paola Gagliardi',
+            'email' => 'paola.gagliardi@gmail.com',
+            'password' => 'paolagagliardi30',
+            'city' => 'Firenze'
+        ],
+        [
+            'name' => 'The Rock Army',
+            'email' => 'rock.army@hotmail.it',
+            'password' => 'rockarmy30',
+            'city' => 'Padova'
+        ],
+        [
+            'name' => 'Nathan Drake',
+            'email' => 'nathan.drake@gmail.com',
+            'password' => 'nathandrake30',
+            'city' => 'Toronto'
+        ],
+        [
+            'name' => 'Jazzy Sons',
+            'email' => 'jazzysons@outlook.com',
+            'password' => 'jazzysons30',
+            'city' => 'Venezia'
+        ],
+   ];
