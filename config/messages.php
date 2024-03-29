9f170d1cc89c01f1f56be1c329eadc4b21a5c510
Drew0004 <drdrewgraphic@gmail.com>
2024-03-29 16:41:25 +0100

    Add messages config and fix messages seeder

---
+++ config/messages.php
@@ -0,0 +1,38 @@
+<?php
+   return[
+    [
+        'firstname' => 'Gino',
+        'lastname' => 'Paoli',
+        'email' => 'gino.paoli@gmail.com',
+        'message' => 'Salve, sono interessato 
+        alla vostra musica e vorrei discutere un 
+        possibile ingaggio per un evento. 
+        Potremmo parlare dei dettagli? Grazie.',
+    ],
+    [
+        'firstname' => 'Vincenzo',
+        'lastname' => 'De Santi',
+        'email' => 'vincenzodesanti@hotmail.it',
+        'message' => 'Ciao, mi piacerebbe collaborare 
+        con te per un progetto musicale. 
+        Possiamo discutere le opzioni? 
+        Attendo tue notizie. Grazie.',
+    ],
+    [
+        'firstname' => 'Francesca',
+        'lastname' => 'Roberti',
+        'email' => 'francesca.roberti87@libero.com',
+        'message' => "Salve, sono interessata alla 
+        vostra musica e vorrei prenotarvi per un'esibizione. 
+        Potremmo discutere disponibilità e tariffe? Grazie.",
+    ],
+    [
+        'firstname' => 'Gianluigi',
+        'lastname' => 'Cecco',
+        'email' => 'gianluigi.cecco@gmail.com',
+        'message' => "Ciao, vorremmo prenotarvi 
+        per un concerto. Possiamo parlare delle 
+        vostre date disponibili e delle tariffe? 
+        Grazie mille.",
+    ],
+   ];
