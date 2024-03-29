941a6bff3889ecf078ea0e8aef88fb9663df304c
Drew0004 <drdrewgraphic@gmail.com>
2024-03-29 16:57:11 +0100

    add 20 users in user config file

--- config/users.php
+++ config/users.php
@@ -30,4 +30,97 @@
             'password' => 'jazzysons30',
             'city' => 'Venezia'
         ],
+        //--- 5
+        [
+            'name' => 'Le Pietre Rotolanti',
+            'email' => 'pietrerotolanti@gmail.com',
+            'password' => 'pietrerotolanti30',
+            'city' => 'Roma',
+        ],
+        [
+            'name' => 'Viaggi Jazz',
+            'email' => 'viaggijazz@outlook.it',
+            'password' => 'viaggijazz30',
+            'city' => 'Milano',
+        ],
+        [
+            'name' => 'Sogni Elettrici',
+            'email' => 'sognielettrici@hotmail.it',
+            'password' => 'sognielettrici30',
+            'city' => 'Bologna',
+        ],
+        [
+            'name' => 'Espresso Funk',
+            'email' => 'espressofunk@cloud.com',
+            'password' => 'espressofunk30',
+            'city' => 'Napoli',
+        ],
+        [
+            'name' => 'Echi Classici',
+            'email' => 'echiclassici@gmail.com',
+            'password' => 'echiclassici30',
+            'city' => 'Firenze',
+        ],
+        //--- 5
+        [
+            'name' => 'Ritmi Reggae',
+            'email' => 'ritmireggae@outlook.it',
+            'password' => 'ritmireggae30',
+            'city' => 'Palermo',
+        ],
+        [
+            'name' => 'Gianluca Rossi',
+            'email' => 'gianlucarossi@hotmail.it',
+            'password' => 'gianlucarossi30',
+            'city' => 'Milano',
+        ],
+        [
+            'name' => 'Eros Leonardi',
+            'email' => 'erosleonardi@gmail.com',
+            'password' => 'erosleonardi30',
+            'city' => 'Roma',
+        ],
+        [
+            'name' => 'Laura Paoletti',
+            'email' => 'laurapaoletti@cloud.com',
+            'password' => 'laurapaoletti30',
+            'city' => 'Bologna',
+        ],
+        [
+            'name' => 'Marco Ricci',
+            'email' => 'marcoricci@gmail.com',
+            'password' => 'marcoricci30',
+            'city' => 'Firenze',
+        ],
+        //--- 5
+        [
+            'name' => 'Alessandro Conti',
+            'email' => 'alessandroconti@hotmail.it',
+            'password' => 'alessandroconti30',
+            'city' => 'Milano',
+        ],
+        [
+            'name' => 'Simone Moretti',
+            'email' => 'simonemoretti@cloud.com',
+            'password' => 'simonemoretti30',
+            'city' => 'Roma',
+        ],
+        [
+            'name' => 'Francesco Ferrari',
+            'email' => 'francescoferrari@gmail.com',
+            'password' => 'francescoferrari30',
+            'city' => 'Bologna',
+        ],
+        [
+            'name' => 'Anna Romano',
+            'email' => 'annaromano@outlook.it',
+            'password' => 'annaromano30',
+            'city' => 'Napoli',
+        ],
+        [
+            'name' => 'Roberto Esposito',
+            'email' => 'robertoesposito@gmail.com',
+            'password' => 'robertoesposito30',
+            'city' => 'Milano',
+        ],
    ];
