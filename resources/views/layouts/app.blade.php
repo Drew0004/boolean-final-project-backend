746e28f84fc7794abb9d94f3bfc254d28d7f81b4
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-04-04 21:44:48 +0200

    push random

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
         <header>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container">
-                    <a class="navbar-brand" href="#nogo">BMusic</a>
+                    <a class="navbar-brand" href="http://localhost:5174/">BMusic</a>
+
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
