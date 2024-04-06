7c4ad97d44d7d5a61c0ec93775dce0e2dd34d6c9
Drew0004 <drdrewgraphic@gmail.com>
2024-04-06 15:31:31 +0200

    Fix hover button forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,52 +10,54 @@
         </ul>
     </div>
     @endif
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div class="my-4">
-            <label class="form-label my-label my-4" for="email">
-                Email
-            </label>
-            <input class="rounded-5 ps-4 form-control @error('email') is-invalid @enderror" type="email" id="email" name="email" placeholder="Inserici la mail..." required>
-            @error('email')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <!-- Password -->
-        <div class="my-4">
-            <label class="form-label my-4 my-label" for="password">
-                Password
-            </label>
-            <input class="rounded-5 ps-4 form-control @error('password') is-invalid @enderror" type="password" id="password" name="password" placeholder="Inserisci la password..." required>
-            @error('password')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <!-- Remember Me -->
-        <div class="my-4">
-            <label for="remember_me">
-                <input class="form-check-input" id="remember_me" type="checkbox" name="remember">
-                <span class="fw-bold my-blue">Remember me</span>
-            </label>
-        </div>
-
-        <div class="my-4">
-            @if (Route::has('password.request'))
-                <a class="my-blue" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-            <button class="ms-5 btn login-btn" type="submit">
-                <span>Log in</span> 
-            </button>
-        </div>
-    </form>
+    <section id="forms">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+    
+            <!-- Email Address -->
+            <div class="my-4">
+                <label class="form-label my-label " for="email">
+                    Email
+                </label>
+                <input class="rounded-5 ps-4 form-control @error('email') is-invalid @enderror" type="email" id="email" name="email" placeholder="Inserici la mail..." required>
+                @error('email')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+    
+            <!-- Password -->
+            <div class="my-4">
+                <label class="form-label  my-label" for="password">
+                    Password
+                </label>
+                <input class="rounded-5 ps-4 form-control @error('password') is-invalid @enderror" type="password" id="password" name="password" placeholder="Inserisci la password..." required>
+                @error('password')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+    
+            <!-- Remember Me -->
+            <div class="my-4">
+                <label for="remember_me">
+                    <input class="form-check-input" id="remember_me" type="checkbox" name="remember">
+                    <span class="fw-bold my-blue">Remember me</span>
+                </label>
+            </div>
+    
+            <div class="my-4">
+                @if (Route::has('password.request'))
+                    <a class="my-blue" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+                <button class="ms-5 btn login-btn" type="submit">
+                    <span>Log in</span> 
+                </button>
+            </div>
+        </form>
+    </section>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,105 +10,107 @@
         </ul>
     </div>
     @endif
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div class="my-4">
-            <label class="form-label my-label" for="name">
-                Nome
-            </label>
-            <input class="rounded-5 ps-4 form-control @error('name') is-invalid @enderror" type="text" id="name" name="name" placeholder="Inserisci il nome..." required maxlength="255">
-            @error('name')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <!-- Email Address -->
-        <div class="my-4">
-            <label class="form-label my-label" for="email">
-                Email
-            </label>
-            <input class="rounded-5 ps-4 form-control @error('email') is-invalid @enderror" type="email" id="email" name="email" placeholder="Inserisci la mail..." required maxlength="255">
-            @error('email')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <!-- Password -->
-        <div class="my-4">
-            <label class="form-label my-label" for="password">
-                Password
-            </label>
-            <input class="rounded-5 ps-4 form-control @error('password') is-invalid @enderror" type="password" id="password" placeholder="Inserisci la password..." name="password" required>
-            @error('password')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="my-4">
-            <label class="form-label my-label" for="password_confirmation">
-                Conferma Password
-            </label>
-            <input class="rounded-5 ps-4 form-control @error('password_confirmation') is-invalid @enderror" type="password" id="password_confirmation" placeholder="Conferma la password..." name="password_confirmation" required>
-            @error('password_confirmation')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <!-- City -->
-        <div class="my-4">
-            <label class="form-label my-label" for="city">
-                Città
-            </label>
-            <input class="rounded-5 ps-4 form-control @error('city') is-invalid @enderror" type="text" id="city" name="city" placeholder="Inserisci una città (es.Milano)..." required maxlength="255">
-            @error('city')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <div class="mb-4">
-                    
-            <div class="form-label my-label">Ruoli</div>
-            @foreach ($roles as $role)
-                <div class="form-check form-check-inline">
-                    <input
-                        class="form-check-input role-checkbox @error('roles') is-invalid @enderror"
-                        type="checkbox"
-                        id="role-{{ $role->id }}"
-                        name="roles[]"
-                        value="{{ $role->id }}"
-                        minlength="1">
-                    <label class="form-check-label fw-bold my-blue" for="role-{{ $role->id }}">{{ $role->title }}</label>
-                </div>
-            @endforeach
-            @error('roles')
-                <div class="alert alert-danger">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-
-        <div class="my-4">
-            <a class="text-decoration-none my-blue" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-        
-            <button class="ms-5 btn login-btn" type="submit">
-                <span>Registrati</span>
-            </button>
+    <section id="forms">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+    
+            <!-- Name -->
+            <div class="my-4">
+                <label class="form-label my-label" for="name">
+                    Nome
+                </label>
+                <input class="rounded-5 ps-4 form-control @error('name') is-invalid @enderror" type="text" id="name" name="name" placeholder="Inserisci il nome..." required maxlength="255">
+                @error('name')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+    
+            <!-- Email Address -->
+            <div class="my-4">
+                <label class="form-label my-label" for="email">
+                    Email
+                </label>
+                <input class="rounded-5 ps-4 form-control @error('email') is-invalid @enderror" type="email" id="email" name="email" placeholder="Inserisci la mail..." required maxlength="255">
+                @error('email')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+    
+            <!-- Password -->
+            <div class="my-4">
+                <label class="form-label my-label" for="password">
+                    Password
+                </label>
+                <input class="rounded-5 ps-4 form-control @error('password') is-invalid @enderror" type="password" id="password" placeholder="Inserisci la password..." name="password" required>
+                @error('password')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+    
+            <!-- Confirm Password -->
+            <div class="my-4">
+                <label class="form-label my-label" for="password_confirmation">
+                    Conferma Password
+                </label>
+                <input class="rounded-5 ps-4 form-control @error('password_confirmation') is-invalid @enderror" type="password" id="password_confirmation" placeholder="Conferma la password..." name="password_confirmation" required>
+                @error('password_confirmation')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+    
+            <!-- City -->
+            <div class="my-4">
+                <label class="form-label my-label" for="city">
+                    Città
+                </label>
+                <input class="rounded-5 ps-4 form-control @error('city') is-invalid @enderror" type="text" id="city" name="city" placeholder="Inserisci una città (es.Milano)..." required maxlength="255">
+                @error('city')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+    
+            <div class="mb-4">
+                        
+                <div class="form-label my-label">Ruoli</div>
+                @foreach ($roles as $role)
+                    <div class="form-check form-check-inline">
+                        <input
+                            class="form-check-input role-checkbox @error('roles') is-invalid @enderror"
+                            type="checkbox"
+                            id="role-{{ $role->id }}"
+                            name="roles[]"
+                            value="{{ $role->id }}"
+                            minlength="1">
+                        <label class="form-check-label fw-bold my-blue" for="role-{{ $role->id }}">{{ $role->title }}</label>
+                    </div>
+                @endforeach
+                @error('roles')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+    
+            <div class="my-4">
+                <a class="text-decoration-none my-blue" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
             
-        </div>
-    </form>
+                <button class="ms-5 btn login-btn" type="submit">
+                    <span>Registrati</span>
+                </button>
+                
+            </div>
+        </form>
+    </section>
 @endsection
