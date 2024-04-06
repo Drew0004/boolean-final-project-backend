3865aa1ca4093dd1664713afca5a50e123f889be
Drew0004 <drdrewgraphic@gmail.com>
2024-04-06 15:24:14 +0200

    Fix registration form style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,11 +14,11 @@
         @csrf
 
         <!-- Name -->
-        <div>
-            <label class="form-label" for="name">
-                Nome*
+        <div class="my-4">
+            <label class="form-label my-label" for="name">
+                Nome
             </label>
-            <input class="form-control @error('name') is-invalid @enderror" type="text" id="name" name="name" required maxlength="255">
+            <input class="rounded-5 ps-4 form-control @error('name') is-invalid @enderror" type="text" id="name" name="name" placeholder="Inserisci il nome..." required maxlength="255">
             @error('name')
                 <div class="alert alert-danger">
                     {{ $message }}
@@ -27,11 +27,11 @@
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
-            <label class="form-label" for="email">
-                Email*
+        <div class="my-4">
+            <label class="form-label my-label" for="email">
+                Email
             </label>
-            <input class="form-control @error('email') is-invalid @enderror" type="email" id="email" name="email" required maxlength="255">
+            <input class="rounded-5 ps-4 form-control @error('email') is-invalid @enderror" type="email" id="email" name="email" placeholder="Inserisci la mail..." required maxlength="255">
             @error('email')
                 <div class="alert alert-danger">
                     {{ $message }}
@@ -40,11 +40,11 @@
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <label class="form-label" for="password">
-                Password*
+        <div class="my-4">
+            <label class="form-label my-label" for="password">
+                Password
             </label>
-            <input class="form-control @error('password') is-invalid @enderror" type="password" id="password" name="password" required>
+            <input class="rounded-5 ps-4 form-control @error('password') is-invalid @enderror" type="password" id="password" placeholder="Inserisci la password..." name="password" required>
             @error('password')
                 <div class="alert alert-danger">
                     {{ $message }}
@@ -53,11 +53,11 @@
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <label class="form-label" for="password_confirmation">
-                Conferma Password*
+        <div class="my-4">
+            <label class="form-label my-label" for="password_confirmation">
+                Conferma Password
             </label>
-            <input class="form-control @error('password_confirmation') is-invalid @enderror" type="password" id="password_confirmation" name="password_confirmation" required>
+            <input class="rounded-5 ps-4 form-control @error('password_confirmation') is-invalid @enderror" type="password" id="password_confirmation" placeholder="Conferma la password..." name="password_confirmation" required>
             @error('password_confirmation')
                 <div class="alert alert-danger">
                     {{ $message }}
@@ -66,11 +66,11 @@
         </div>
 
         <!-- City -->
-        <div class="mt-4">
-            <label class="form-label" for="city">
-                Città*
+        <div class="my-4">
+            <label class="form-label my-label" for="city">
+                Città
             </label>
-            <input class="form-control @error('city') is-invalid @enderror" type="text" id="city" name="city" placeholder="Inserisci una città (es.Milano)..." required maxlength="255">
+            <input class="rounded-5 ps-4 form-control @error('city') is-invalid @enderror" type="text" id="city" name="city" placeholder="Inserisci una città (es.Milano)..." required maxlength="255">
             @error('city')
                 <div class="alert alert-danger">
                     {{ $message }}
@@ -78,19 +78,19 @@
             @enderror
         </div>
 
-        <div>
+        <div class="mb-4">
                     
-            <div class="form-label">Ruoli*</div>
+            <div class="form-label my-label">Ruoli</div>
             @foreach ($roles as $role)
                 <div class="form-check form-check-inline">
                     <input
-                        class="form-check-input @error('roles') is-invalid @enderror"
+                        class="form-check-input role-checkbox @error('roles') is-invalid @enderror"
                         type="checkbox"
                         id="role-{{ $role->id }}"
                         name="roles[]"
                         value="{{ $role->id }}"
                         minlength="1">
-                    <label class="form-check-label" for="role-{{ $role->id }}">{{ $role->title }}</label>
+                    <label class="form-check-label fw-bold my-blue" for="role-{{ $role->id }}">{{ $role->title }}</label>
                 </div>
             @endforeach
             @error('roles')
@@ -100,13 +100,13 @@
             @enderror
         </div>
 
-        <div>
-            <a class="text-decoration-none" href="{{ route('login') }}">
+        <div class="my-4">
+            <a class="text-decoration-none my-blue" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
         
-            <button class="ms-2 btn btn-primary" type="submit">
-                Registrati
+            <button class="ms-5 btn login-btn" type="submit">
+                <span>Registrati</span>
             </button>
             
         </div>
