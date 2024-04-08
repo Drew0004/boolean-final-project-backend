27db36aa0989c1a30e6f10ba0ddf3c2dca4c6a1b
Drew0004 <drdrewgraphic@gmail.com>
2024-04-08 12:00:44 +0200

    Add schedule command for deleting sponsorships, doing manual

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('sponsors:delete-expired')->daily();
     }
 
     /**
@@ -25,3 +25,5 @@ class Kernel extends ConsoleKernel
         require base_path('routes/console.php');
     }
 }
+
+//codice da eseguire su terminale per cancellazione manuale delle sponsorizzazioni php artisan sponsors:delete-expired
