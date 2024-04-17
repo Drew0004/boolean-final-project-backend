df445f4e8e8531f9a1867e6a86adcb85fa4826ea
Drew0004 <drdrewgraphic@gmail.com>
2024-04-17 18:00:46 +0200

    Fix head titles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         {{-- Font awesome --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <title>@yield('page-title') | {{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('page-title')</title>
 
         {{-- Braintree --}}
         <script src="https://js.braintreegateway.com/web/dropin/1.42.0/js/dropin.min.js"></script>
